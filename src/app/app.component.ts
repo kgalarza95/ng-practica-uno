6cf2b5094911ebfa20f0d32ededbb6e25326deeb
Kevin Galarza Vera <44540130+kgalarza95@users.noreply.github.com>
2023-11-23 23:40:44 -0500

    se mas información

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hola-mundo-ng';
+
+  codigoUno = `import { Component } from '@angular/core';
+  @ Component (
+        selector: 'app-root',
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
+      )
+      export class AppComponent {
+        title = 'Mi Aplicación Angular';
+      }";`
+
+  codigoDos = `<div *ngIf="mostrarMensaje">
+              <p>{{ mensaje }}</p>
+            </div>
+        
+         
+            <ul>
+              <li *ngFor="let item of listaItems">{{ item }}</li>
+            </ul>`
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './general/header/header.component';
+import { FooterComponent } from './general/footer/footer.component';
+import { NavbarComponent } from './general/navbar/navbar.component';
+import { JumbotronComponent } from './general/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule
