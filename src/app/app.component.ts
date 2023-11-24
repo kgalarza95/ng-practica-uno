68b82c26f2c91fcb9a1f323f495e2b3a0e8c93a7
Kevin Galarza Vera <44540130+kgalarza95@users.noreply.github.com>
2023-11-24 00:15:41 -0500

    se agrega prueba de ndif y ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,4 +26,14 @@ export class AppComponent {
             <ul>
               <li *ngFor="let item of listaItems">{{ item }}</li>
             </ul>`
+
+
+  mostrarIntroduccion = true;
+  mostrarCaracteristicas = true;
+  caracteristicas = [
+    'Plantilla (Template): Los componentes tienen una plantilla asociada que define la estructura HTML de la interfaz de usuario.',
+    'Estilos: Pueden tener estilos CSS específicos asociados.',
+    'Lógica: Contienen la lógica de la aplicación en el archivo TypeScript asociado.',
+    'Metadatos (Metadata): Los metadatos del componente, como decoradores, proporcionan información adicional sobre el componente.'
+  ];
 }
